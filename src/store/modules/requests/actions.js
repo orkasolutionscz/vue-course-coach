8c9a8acad28b427bc6f8454d1289d55d89f35d8e
jiri.fait <fait@orkasolutions.cz>
2021-07-15 20:36:31 +0200

    lekce 252 nedodelana

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,36 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
             coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
+        
+        const response = await fetch(`https://vue-http-demo-655e7-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`, {
+            method: "POST",
+            body: JSON.stringify(newRequest)
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Fetch error. Fail to send request');
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
+        
         context.commit('addRequest', newRequest)
+    },
+
+    async fetchRequest(context) {
+        const coachId = context.rootGetters.userId;
+        const response = await fetch(`https://vue-http-demo-655e7-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Fetch error. Fail to send requests');
+            throw error;
+        }
+      
     }
 };
