7f4ae129a9aa89946c6da070dea9ab43ce45459a
jiri.fait <fait@orkasolutions.cz>
2021-07-15 17:12:07 +0200

    Finalni backend

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-REQUESTS
-    </div>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in receivedRequest"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any request yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-    export default {
-        
-    }
+import RequestItem from './component/RequestItem.vue';
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequest() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+};
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
